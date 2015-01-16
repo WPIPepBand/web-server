9578c5fc7a6c280c2b5a2cc4978ed73e42d9dca6
Thomas Clark <tjclark@wpi.edu>
2015-01-15 21:35:21 -0500

    Removed submodule update command from build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ source $1
 echo "Config for the cgi-bin: $cgi_bin" >&2
 echo "config for the htdocs: $htdocs" >&2
 
-git submodule update --remote ./public_html/cgi-bin/pages
+#git submodule update --remote ./public_html/cgi-bin/pages
 go build -o "$cgi_bin"make-page.cgi ./make-page.go
