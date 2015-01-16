d3c747fc9e61eb9d646babb5c9832c165adbc7c7
Arthur Lockman <hello@rthr.me>
2015-01-15 20:25:56 -0500

    Added server configuration tool.

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
-go build -o ./public_html/cgi-bin/make-page.cgi ./make-page.go
+#!/bin/bash
+echo "Reading config." >&2
+source $1
+echo "Config for the cgi-bin: $cgi_bin" >&2
+echo "config for the htdocs: $htdocs" >&2
+
+go build -o "$cgi_bin"make-page.cgi ./make-page.go
