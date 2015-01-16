ea5f4ef68bfd3e3079bde41e21382a99f954b9ab
Arthur Lockman <hello@rthr.me>
2015-01-15 21:16:33 -0500

    Updated build script to actually pull content changes.

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-git submodule update
-
 echo "Reading config." >&2
 source $1
 echo "Config for the cgi-bin: $cgi_bin" >&2
 echo "config for the htdocs: $htdocs" >&2
 
+git submodule update --remote ./public_html/cgi-bin/pages
 go build -o "$cgi_bin"make-page.cgi ./make-page.go
