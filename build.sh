c75dee410af17be86383d70a3bbe104b1ad38d49
jtgrotz <37751533+jtgrotz@users.noreply.github.com>
2018-07-18 15:15:28 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,6 +7,8 @@ echo "config for the htdocs: $htdocs" >&2
 #git submodule update --remote ./public_html/cgi-bin/pages
 go build -o "$cgi_bin"make-page.cgi ./make-page.go
 go build -o "$cgi_bin"anon-sugg.cgi ./anon-suggestion.go
+go build -o "$cgi_bin"name-sugg.cgi ./name-suggestion.go
+go build -o "$cgi_bin"advisor-sugg.cgi ./advisor-suggestion.go
 
 cp -r public_html/* $htdocs
 
